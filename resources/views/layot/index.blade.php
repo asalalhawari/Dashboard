25386671b5030e34670b4c8d0025dabe16f6656a
ASAL HAWARI <ASALHAWARI2000@gmail.com>
2024-08-18 23:22:12 +0300

    first commit

---
+++ resources/views/layot/index.blade.php
@@ -0,0 +1,19 @@
+
+@include('layot.header')
+@include('layot.navbar')
+@include('layot.sidebar')
+@yield('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="copyright">
+            <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+        </div>
+    </div>
+</div>
+@extends('layot.footer')
+
+
+  <!-- Jquery JS-->
+
+  
+<!-- end document-->
